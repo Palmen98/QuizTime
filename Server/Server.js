893f641124d3c0e3b5d124b3bf3eb44e34f505af
Palmen98 <simon.palm98@hotmail.se>
2022-02-04 11:38:57 +0100

    Server can acess the api path.

--- Server/Server.js
+++ Server/Server.js
@@ -1,13 +1,20 @@
-import bodyParser from 'body-parser';
+/**
+ * @Palmen98
+ * Starting point for the express server.
+ * @version 1.0.0
+ */
+
 import express from 'express';
+import questionsAPI from './Routes/QuestionsRoute.js'
 
 const app = express();
 const port = process.env.PORT || 5000
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
 
 // Questions
-app.get('/api', questions);
+app.use('/api', questionsAPI);
+
+app.get('/', (req, res) => res.send('Welcome to quizGame'))
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
